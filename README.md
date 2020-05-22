ca94bc04c0a180829f273397beee16af14074d0f
Ivan0xFF <me@ivanff.com>
2020-05-21 23:47:50 -0700

    Added sign/item frame passthrough feature

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# Sign Editor
-This mod lets you edit signs after they are placed, without having to break the signs. Works for fabric servers.
+# Sign Editor & Passthrough
+This mod is only for Fabric Servers and requires [Fabric API](https://www.curseforge.com/minecraft/mc-mods/fabric-api)
 
-**To use:** sneak + click the sign to bring up the sign editor.
+### Features
+ - **Sign Editor:** Edit placed signs by shift(sneak) + right-click
+ - **Sign & Item Frame Passthrough:** Access chests, shulker boxes, barrels, etc behind signs by right-clicking the sign.
+ - Rotate Item Frames by shift(sneak) + right-click
+
+### Shout-outs
+Special thanks to Girafi for the idea for Passthrough Signs. Find their original mod for Forge [here](https://www.curseforge.com/minecraft/mc-mods/passthrough-signs)
 
  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
--- src/main/java/com/ivanff/signEditor/SignEditorMod.java
+++ src/main/java/com/ivanff/signEditor/SignEditorMod.java
@@ -2,11 +2,21 @@ package com.ivanff.signEditor;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.SignBlockEntity;
+import net.minecraft.entity.decoration.ItemFrameEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 
 import org.apache.logging.log4j.Logger;
 
@@ -28,8 +38,8 @@ public class SignEditorMod implements ModInitializer {
             BlockPos pos = hitResult.getBlockPos();
             BlockEntity blockEntity = world.getBlockEntity(pos);
             if (blockEntity instanceof SignBlockEntity) {
-                SignBlockEntity signBlock = (SignBlockEntity) blockEntity;
                 if (player.isSneaking()) {
+                    SignBlockEntity signBlock = (SignBlockEntity) blockEntity;
                     ((SignEntityMixin) signBlock).setSignEditable(true);
                     if (signBlock.isEditable()) {
                         signBlock.setEditor(player);
@@ -37,9 +47,37 @@ public class SignEditorMod implements ModInitializer {
                     } else {
                         player.sendMessage(new LiteralText("Sign is not editable"));
                     }
+                } else {
+                    BlockState state = world.getBlockState(pos);
+                    if (state.contains(HorizontalFacingBlock.FACING)) {
+                        Direction oppositeDirection = state.get(HorizontalFacingBlock.FACING).getOpposite();
+                        handlePassthrough(player, world, hand, pos, oppositeDirection);
+                    }
                 }
             }
             return ActionResult.PASS;
         });
+
+        UseEntityCallback.EVENT.register((player, world, hand, entity, hitResult) -> {
+            if (entity instanceof ItemFrameEntity) {
+                if (!player.isSneaking()) {
+                    ItemFrameEntity itemFrame = (ItemFrameEntity) entity;
+                    // TODO: Figure out a less hacy way to disable rotation.
+                    itemFrame.setRotation(itemFrame.getRotation() - 1);
+                    BlockPos pos = entity.getBlockPos();
+                    Direction oppositeDirection = entity.getHorizontalFacing().getOpposite();
+                    handlePassthrough(player, world, hand, pos, oppositeDirection);
+                }
+            }
+            return ActionResult.PASS;
+        });
+    }
+
+    void handlePassthrough(PlayerEntity player, World world, Hand hand, BlockPos pos, Direction oppositeDirection) {
+        BlockPos hangingPos = pos.add(oppositeDirection.getOffsetX(), oppositeDirection.getOffsetY(), oppositeDirection.getOffsetZ());
+        BlockState hangingState = world.getBlockState(hangingPos);
+        Vec3d hanginPosVec3d = new Vec3d(hangingPos.getX(), hangingPos.getY(), hangingPos.getZ());
+        BlockHitResult hangingHitResult = new BlockHitResult(hanginPosVec3d, oppositeDirection, pos, false); 
+        hangingState.getBlock().onUse(hangingState, world, hangingPos, player, hand, hangingHitResult);
     }
 }
